7f504e3f83249b4539883609ec9b3de4b41959f2
Sujan <sujan.maharjan75@gmail.com>
2020-11-18 23:08:46 +0545

    Animation added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,57 +1,79 @@
 $(window).on("load",function(){
     $(".c-loader").fadeOut();
-
+    $(".block-text").hover(function(){
+      var image = $(this).data("image");
+      $("."+image).css("opacity",1);
+      $(".c-background").addClass("image-active");
+    },function(){
+      $(".image").css("opacity",0);
+      $(".c-background").removeClass("image-active");
+    });
     anime({
         targets: '.floating-image .image-1',
         translateX: 20,
         easing: 'easeInOutSine'
       });
 
-      anime({
-        targets: '.floating-image .image-2',
-        translateX: -20,
-        easing: 'easeInOutSine'
-      });
+    anime({
+      targets: '.floating-image .image-2',
+      translateX: -20,
+      easing: 'easeInOutSine'
+    });
 
-      anime({
-        targets: '.footer-logo',
-        opacity:1,
-        translateY: -20,
-        easing: 'easeInOutSine',
-        delay: 1000
-      });
+    anime({
+      targets: '.footer-logo',
+      opacity:1,
+      translateY: -20,
+      easing: 'easeInOutSine',
+      delay: 1000
+    });
 
+    anime({
+      targets: '.c-title',
+      opacity:1,
+      translateY: 20,
+      easing: 'easeInOutSine',
+      delay: 1000
+    });
+
+  anime({
+      targets: '.bl',
+      opacity:1,
+      easing: 'easeInElastic(1, .6)',
+      delay: anime.stagger(100)
+    });
+
+    //load overlay
+    $(".block-text a").on("click",function(e){
+      e.preventDefault();
+      var _targetId = $(this).attr("href");
+      $(".c-overlay").animate({"max-width": "100%"},500,function(){
+        $(document).find(".tab-item"+_targetId).fadeIn(100,function(){
+          anime({
+            targets: '.c-overlay .animate',
+            translateY: -20,
+            opacity:1,
+            easing: 'easeInOutSine',
+            delay: anime.stagger(50)
+          });
+         
+        });
+      });
+    })
+
+    $(document).on("click", ".overlay-close", function(e){
+      e.preventDefault();
       anime({
-        targets: '.c-title',
-        opacity:1,
+        targets: '.c-overlay .animate',
         translateY: 20,
+        opacity:0,
         easing: 'easeInOutSine',
-        delay: 1000
+        complete: function(anim) {
+                    
+          $(".tab-item").fadeOut(500,function(){ $(".c-overlay").animate({"max-width":"0%"}, 500)});
+        }
       });
-
-
-      anime({
-        targets: '.block-1',
-        opacity:1,
-        easing: 'easeInElastic(1, .6)',
-        delay: 100
-      });
-      anime({
-        targets: '.block-2',
-        opacity:1,
-        easing: 'easeInElastic(1, .6)',
-        delay: 500
-      });
-      anime({
-        targets: '.block-3',
-        opacity:3,
-        easing: 'easeInElastic(1, .6)',
-        delay:1000
-      });
-      anime({
-        targets: '.block-4',
-        opacity:1,
-        easing: 'easeInElastic(1, .6)',
-        delay: 1500
-      });
+     
+      
+    })
 });
